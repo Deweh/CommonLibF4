a261a861fea34bf821ef674a353b3096ef9d7aa8
DeweySalt <76853940+Deweh@users.noreply.github.com>
2023-09-16 14:52:33 -0400

    NiQuaternion util

--- CommonLibF4/include/RE/NetImmerse/NiQuaternion.h
+++ CommonLibF4/include/RE/NetImmerse/NiQuaternion.h
@@ -5,22 +5,37 @@ namespace RE
 	class NiQuaternion
 	{
 	public:
-		float w;  // 00
-		float x;  // 04
-		float y;  // 08
-		float z;  // 0C
+		float w{ 0.0f };  // 00
+		float x{ 0.0f };  // 04
+		float y{ 0.0f };  // 08
+		float z{ 0.0f };  // 0C
 
-		void ToRotation(NiMatrix3* a_rotation) const {
+		void FromRotation(const NiMatrix3& a_rotation) const
+		{
+			using func_t = decltype(&NiQuaternion::FromRotation);
+			REL::Relocation<func_t> func{ REL::ID(616807) };
+			return func(this, a_rotation);
+		}
+
+		void ToRotation(NiMatrix3& a_rotation) const
+		{
 			using func_t = decltype(&NiQuaternion::ToRotation);
 			REL::Relocation<func_t> func{ REL::ID(1379385) };
 			return func(this, a_rotation);
 		}
 
-		//x = pitch, y = roll, z = yaw
-		void FromEulerAnglesXYZ(float a_X, float a_Y, float a_Z) {
+		void FromEulerAnglesXYZ(float a_X, float a_Y, float a_Z)
+		{
 			using func_t = decltype(&NiQuaternion::FromEulerAnglesXYZ);
 			REL::Relocation<func_t> func{ REL::ID(896501) };
 			return func(this, a_X, a_Y, a_Z);
+		}
+
+		void Slerp(float a_time, const NiQuaternion& a_begin, const NiQuaternion& a_end)
+		{
+			using func_t = decltype(&NiQuaternion::Slerp);
+			REL::Relocation<func_t> func{ REL::ID(1338018) };
+			return func(this, a_time, a_begin, a_end);
 		}
 	};
 	static_assert(sizeof(NiQuaternion) == 0x10);
