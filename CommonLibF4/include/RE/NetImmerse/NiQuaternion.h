23681a48cc61d03aab6b0bef2b2f30704ba0b542
DeweySalt <76853940+Deweh@users.noreply.github.com>
2023-11-19 05:26:54 -0500

    fix `NiQuaternion` constness

--- CommonLibF4/include/RE/NetImmerse/NiQuaternion.h
+++ CommonLibF4/include/RE/NetImmerse/NiQuaternion.h
@@ -38,7 +38,7 @@ namespace RE
 			return func(this, angle, axis);
 		}
 
-		void ToAngleAxis(float& angle, RE::NiPoint3& axis)
+		void ToAngleAxis(float& angle, RE::NiPoint3& axis) const
 		{
 			using func_t = decltype(&NiQuaternion::ToAngleAxis);
 			REL::Relocation<func_t> func{ REL::ID(1082915) };
@@ -66,15 +66,15 @@ namespace RE
 			return func(this, q);
 		}
 
-		RE::NiQuaternion Invert() {
+		RE::NiQuaternion Invert() const {
 			return { -w, x, y, z };
 		}
 
-		RE::NiQuaternion Abs() {
+		RE::NiQuaternion Abs() const {
 			return { w < 0.0 ? -w : w, x, y, z };
 		}
 
-		NiQuaternion operator*(const NiQuaternion& a_rhs)
+		NiQuaternion operator*(const NiQuaternion& a_rhs) const
 		{
 			return {
 				a_rhs.w * w - a_rhs.x * x - a_rhs.y * y - a_rhs.z * z,
@@ -88,27 +88,27 @@ namespace RE
 			(*this) = operator*(a_rhs);
 		}
 
-		NiQuaternion operator*(float s)
+		NiQuaternion operator*(float s) const
 		{
 			return { w * s, x * s, y * s, z * s };
 		}
 
-		NiQuaternion operator+(const NiQuaternion& a_rhs)
+		NiQuaternion operator+(const NiQuaternion& a_rhs) const
 		{
 			return { w + a_rhs.w, x + a_rhs.x, y + a_rhs.y, z + a_rhs.z };
 		}
 
-		RE::NiQuaternion operator-(const RE::NiQuaternion& a_rhs)
+		RE::NiQuaternion operator-(const RE::NiQuaternion& a_rhs) const
 		{
 			return { w - a_rhs.w, x - a_rhs.x, y - a_rhs.y, z - a_rhs.z };
 		}
 
-		RE::NiQuaternion operator/(float s)
+		RE::NiQuaternion operator/(float s) const
 		{
 			return { w / s, x / s, y / s, z / s };
 		}
 
-		RE::NiQuaternion operator-()
+		RE::NiQuaternion operator-() const
 		{
 			return { -w, -x, -y, -z };
 		}
